9fdfe7e7265dcf24196479d0f94679852a4311d5
SunWoong <sunwoong1122@gmail.com>
2022-07-27 15:55:29 +0900

    feat: 색상 설정 & bottom navigation 구현

--- app/src/main/java/com/greenfriends/zeroway/MainActivity.kt
+++ app/src/main/java/com/greenfriends/zeroway/MainActivity.kt
@@ -2,10 +2,62 @@ package com.greenfriends.zeroway
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.greenfriends.zeroway.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initBottomNavigation()
+    }
+
+    private fun initBottomNavigation() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_fl, HomeFragment())
+            .commitAllowingStateLoss()
+
+        binding.mainBnv.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.homeFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_fl, HomeFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.challengeFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_fl, ChallengeFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.storeFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_fl, StoreFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.communityFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_fl, CommunityFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.userFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_fl, UserFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+            }
+            false
+        }
     }
 }
