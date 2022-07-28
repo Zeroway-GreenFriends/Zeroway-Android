9979340c9f94084699c9611e1a1aa87036ce266b
MinJaeSon <mjsonsj@gmail.com>
2022-07-29 01:33:40 +0900

    feat: init ui

---
+++ Zeroway-Android/app/src/main/java/com/greenfriends/zeroway/ChallengeFragment.kt
@@ -0,0 +1,21 @@
+package com.greenfriends.zeroway
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.greenfriends.zeroway.databinding.FragmentChallengeBinding
+
+class ChallengeFragment : Fragment() {
+    private lateinit var binding: FragmentChallengeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentChallengeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
