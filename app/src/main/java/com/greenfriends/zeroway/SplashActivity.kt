2ad0fe010bee2f39939219ee206108be3ad95869
MinJaeSon <mjsonsj@gmail.com>
2022-08-02 18:22:05 +0900

    FEAT: 챌린지 UI & Splash

---
+++ app/src/main/java/com/greenfriends/zeroway/SplashActivity.kt
@@ -0,0 +1,28 @@
+package com.greenfriends.zeroway
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+            finish()
+        },DURATION)
+
+    }
+    companion object {
+        private const val DURATION : Long = 3000
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
+}
