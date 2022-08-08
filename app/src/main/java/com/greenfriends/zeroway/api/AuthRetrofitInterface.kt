56edc03b0adba9be6eb2aed5bb7f64c7d328cdd5
SunWoong <sunwoong1122@gmail.com>
2022-08-08 18:32:31 +0900

    feat: login interface & login API 구현

---
+++ app/src/main/java/com/greenfriends/zeroway/api/AuthRetrofitInterface.kt
@@ -0,0 +1,18 @@
+package com.greenfriends.zeroway.api
+
+import com.greenfriends.zeroway.data.LoginResponse
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
+
+interface AuthRetrofitInterface {
+    @Multipart
+    @POST("user/auth/login")
+    fun login(
+        @Part profileImg: MultipartBody.Part,
+        @Part("signInReq") user: RequestBody
+    ): Call<LoginResponse>
+}
---
+++ app/src/main/java/com/greenfriends/zeroway/api/LoginView.kt
@@ -0,0 +1,6 @@
+package com.greenfriends.zeroway.api
+
+interface LoginView {
+    fun onLoginSuccess()
+    fun onLoginFailure()
+}
