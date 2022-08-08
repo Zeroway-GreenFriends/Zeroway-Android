c4e3a366674392ee834d80aa0026cf3701bb7456
SunWoong <sunwoong1122@gmail.com>
2022-08-08 15:24:07 +0900

    feat: naver login 구현

--- app/src/main/java/com/greenfriends/zeroway/LoginActivity.kt
+++ app/src/main/java/com/greenfriends/zeroway/LoginActivity.kt
@@ -3,8 +3,13 @@ package com.greenfriends.zeroway
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.greenfriends.zeroway.databinding.ActivityLoginBinding
-import com.greenfriends.zeroway.databinding.ActivityMainBinding
+import com.navercorp.nid.NaverIdLoginSDK
+import com.navercorp.nid.oauth.NidOAuthLogin
+import com.navercorp.nid.oauth.OAuthLoginCallback
+import com.navercorp.nid.profile.NidProfileCallback
+import com.navercorp.nid.profile.data.NidProfileResponse
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
@@ -12,19 +17,60 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
-
-        binding.loginKakaoLoginIv.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
-        }
-
-        binding.loginGoogleLoginIv.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
-        }
-
-        binding.loginNaverLoginIv.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
-        }
-
         setContentView(binding.root)
+
+        onClickNaverLogin()
+    }
+
+    private fun onClickNaverLogin() {
+        binding.loginNaverLoginIv.setOnClickListener {
+            naverLogin()
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+
+    private fun initializeNaverClient() {
+        val naverClientId = getString(R.string.naver_client_id)
+        val naverClientSecret = getString(R.string.naver_client_secret)
+        val naverClientName = getString(R.string.app_name)
+        NaverIdLoginSDK.initialize(this, naverClientId, naverClientSecret, naverClientName)
+    }
+
+    private fun naverLogin() {
+        initializeNaverClient()
+        lateinit var naverToken: String
+        val profileCallback = object : NidProfileCallback<NidProfileResponse> {
+            override fun onSuccess(result: NidProfileResponse) {
+                val userId = result.profile?.id
+                Log.d("NAVER/LOGIN/SUCCESS", "id:${userId}, token: $naverToken")
+            }
+
+            override fun onFailure(httpStatus: Int, message: String) {
+                val errorCode = NaverIdLoginSDK.getLastErrorCode().code
+                val errorDescription = NaverIdLoginSDK.getLastErrorDescription()
+                Log.d("NAVER/LOGIN/FAILURE", "errorCode: ${errorCode}, errorDescription: $errorDescription")
+            }
+
+            override fun onError(errorCode: Int, message: String) {
+                onFailure(errorCode, message)
+            }
+        }
+        val oAuthLoginCallback = object : OAuthLoginCallback {
+            override fun onSuccess() {
+                naverToken = NaverIdLoginSDK.getAccessToken().toString()
+                NidOAuthLogin().callProfileApi(profileCallback)
+            }
+
+            override fun onFailure(httpStatus: Int, message: String) {
+                val errorCode = NaverIdLoginSDK.getLastErrorCode().code
+                val errorDescription = NaverIdLoginSDK.getLastErrorDescription()
+                Log.d("NAVER/LOGIN/FAILURE", "errorCode: ${errorCode}, errorDescription: $errorDescription")
+            }
+
+            override fun onError(errorCode: Int, message: String) {
+                onFailure(errorCode, message)
+            }
+        }
+        NaverIdLoginSDK.authenticate(this, oAuthLoginCallback)
     }
 }
