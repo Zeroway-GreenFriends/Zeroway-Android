3f1baca62d0cd13da91535e1bde19e02f8fc968a
SunWoong <sunwoong1122@gmail.com>
2022-08-09 16:39:04 +0900

    feat: 기존 login API service 수정 & signUp API service 구현

--- app/src/main/java/com/greenfriends/zeroway/api/AuthService.kt
+++ app/src/main/java/com/greenfriends/zeroway/api/AuthService.kt
@@ -1,7 +1,7 @@
 package com.greenfriends.zeroway.api
 
 import android.util.Log
-import com.greenfriends.zeroway.data.LoginResponse
+import com.greenfriends.zeroway.data.AuthResponse
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.Call
@@ -11,25 +11,48 @@ import retrofit2.Response
 class AuthService {
     private var retrofit = RetrofitClient.getRetrofit()
     private lateinit var loginView: LoginView
+    private lateinit var signUpView: SignUpView
 
     fun setLoginView(loginView: LoginView) {
         this.loginView = loginView
     }
 
-    fun login(profileImg: MultipartBody.Part, user: RequestBody) {
+    fun setSignUpView(signUpView: SignUpView) {
+        this.signUpView = signUpView
+    }
+
+    fun login(email: String) {
         val authService = retrofit?.create(AuthRetrofitInterface::class.java)
-        authService!!.login(profileImg, user).enqueue(object : Callback<LoginResponse> {
-            override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
-                Log.d("LOGIN/SUCCESS", response.body().toString())
+        authService!!.login(email).enqueue(object : Callback<AuthResponse> {
+            override fun onResponse(call: Call<AuthResponse>, response: Response<AuthResponse>) {
+                Log.d("API/LOGIN/SUCCESS", response.body().toString())
                 if (response.isSuccessful) {
-                    loginView.onLoginSuccess()
+                    loginView.onLoginSuccess(response.body()!!.result)
                 } else {
                     loginView.onLoginFailure()
                 }
             }
 
-            override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
-                Log.d("LOGIN/FAILURE", t.message.toString())
+            override fun onFailure(call: Call<AuthResponse>, t: Throwable) {
+                Log.d("API/LOGIN/FAILURE", t.message.toString())
+            }
+        })
+    }
+
+    fun signUp(profileImg: MultipartBody.Part, user: RequestBody) {
+        val authService = retrofit?.create(AuthRetrofitInterface::class.java)
+        authService!!.signUp(profileImg, user).enqueue(object : Callback<AuthResponse> {
+            override fun onResponse(call: Call<AuthResponse>, response: Response<AuthResponse>) {
+                Log.d("API/SIGNUP/SUCCESS", response.body().toString())
+                if (response.isSuccessful) {
+                    signUpView.onSignUpSuccess(response.body()!!.result)
+                } else {
+                    signUpView.onSignUpFailure()
+                }
+            }
+
+            override fun onFailure(call: Call<AuthResponse>, t: Throwable) {
+                Log.d("API/SIGNUP/FAILURE", t.message.toString())
             }
         })
     }
