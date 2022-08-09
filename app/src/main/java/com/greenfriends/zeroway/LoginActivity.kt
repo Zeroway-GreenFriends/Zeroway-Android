a6065cc246b3bc217e3ac549e83bcb1fbbd81f7f
SunWoong <sunwoong1122@gmail.com>
2022-08-09 16:42:07 +0900

    feat: login API 기능 구현

--- app/src/main/java/com/greenfriends/zeroway/LoginActivity.kt
+++ app/src/main/java/com/greenfriends/zeroway/LoginActivity.kt
@@ -1,9 +1,12 @@
 package com.greenfriends.zeroway
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.greenfriends.zeroway.api.AuthService
+import com.greenfriends.zeroway.api.LoginView
+import com.greenfriends.zeroway.data.Result
 import com.greenfriends.zeroway.databinding.ActivityLoginBinding
 import com.navercorp.nid.NaverIdLoginSDK
 import com.navercorp.nid.oauth.NidOAuthLogin
@@ -11,9 +14,10 @@ import com.navercorp.nid.oauth.OAuthLoginCallback
 import com.navercorp.nid.profile.NidProfileCallback
 import com.navercorp.nid.profile.data.NidProfileResponse
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), LoginView {
     private lateinit var binding: ActivityLoginBinding
     private var email: String = ""
+    private var provider: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,10 +27,17 @@ class LoginActivity : AppCompatActivity() {
         onClickNaverLogin()
     }
 
-    private fun onClickNaverLogin() {
-        binding.loginNaverLoginIv.setOnClickListener {
-            naverLogin()
-        }
+    private fun setEmail(email: String) {
+        this.email = email
+    }
+
+    private fun setProvider(provider: String) {
+        this.provider = provider
+    }
+
+    private fun startMainActivity() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 
     private fun startSignUpActivity() {
@@ -35,6 +46,18 @@ class LoginActivity : AppCompatActivity() {
         intent.putExtra("provider", "NAVER")
         startActivity(intent)
         finish()
+    }
+
+    private fun login() {
+        val authService = AuthService()
+        authService.setLoginView(this)
+        authService.login(email)
+    }
+
+    private fun onClickNaverLogin() {
+        binding.loginNaverLoginIv.setOnClickListener {
+            naverLogin()
+        }
     }
 
     private fun initializeNaverClient() {
@@ -51,15 +74,19 @@ class LoginActivity : AppCompatActivity() {
         val profileCallback = object : NidProfileCallback<NidProfileResponse> {
             override fun onSuccess(result: NidProfileResponse) {
                 val userId = result.profile?.id
-                email = result.profile?.email.toString()
+                setEmail(result.profile?.email.toString())
+                setProvider("NAVER")
+                login()
                 Log.d("NAVER/LOGIN/SUCCESS", "id:${userId}, token: $naverToken")
-                startSignUpActivity()
             }
 
             override fun onFailure(httpStatus: Int, message: String) {
                 val errorCode = NaverIdLoginSDK.getLastErrorCode().code
                 val errorDescription = NaverIdLoginSDK.getLastErrorDescription()
-                Log.d("NAVER/LOGIN/FAILURE", "errorCode: ${errorCode}, errorDescription: $errorDescription")
+                Log.d(
+                    "NAVER/LOGIN/FAILURE",
+                    "errorCode: ${errorCode}, errorDescription: $errorDescription"
+                )
             }
 
             override fun onError(errorCode: Int, message: String) {
@@ -76,7 +103,10 @@ class LoginActivity : AppCompatActivity() {
             override fun onFailure(httpStatus: Int, message: String) {
                 val errorCode = NaverIdLoginSDK.getLastErrorCode().code
                 val errorDescription = NaverIdLoginSDK.getLastErrorDescription()
-                Log.d("NAVER/LOGIN/FAILURE", "errorCode: ${errorCode}, errorDescription: $errorDescription")
+                Log.d(
+                    "NAVER/LOGIN/FAILURE",
+                    "errorCode: ${errorCode}, errorDescription: $errorDescription"
+                )
             }
 
             override fun onError(errorCode: Int, message: String) {
@@ -84,5 +114,18 @@ class LoginActivity : AppCompatActivity() {
             }
         }
         NaverIdLoginSDK.authenticate(this, oAuthLoginCallback)
+    }
+
+    override fun onLoginSuccess(result: Result) {
+        Log.d("LOGIN/SUCCESS", "기존 회원 로그인 성공")
+        // jwt 저장하는 코드를 추가해야 하는 곳입니다.
+        startMainActivity()
+        finish()
+    }
+
+    override fun onLoginFailure() {
+        Log.d("LOGIN/FAILURE", "기존 회원 로그인 실패 -> 미가입 회원으로 신규 가입이 필요합니다.")
+        startSignUpActivity()
+        finish()
     }
 }
