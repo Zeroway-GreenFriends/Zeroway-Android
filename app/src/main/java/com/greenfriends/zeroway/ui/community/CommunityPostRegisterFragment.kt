fc5b0a68e6f2cd53697350c190422d940877ebf5
SunWoong <sunwoong1122@gmail.com>
2022-08-13 18:38:43 +0900

    feat: community 게시물 사진 recycler view adapter 구현 & 적용

--- app/src/main/java/com/greenfriends/zeroway/ui/community/CommunityPostRegisterFragment.kt
+++ app/src/main/java/com/greenfriends/zeroway/ui/community/CommunityPostRegisterFragment.kt
@@ -5,9 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.greenfriends.zeroway.databinding.FragmentCommunityPostRegisterBinding
+import com.greenfriends.zeroway.ui.common.ViewModelFactory
 
-class CommunityPostRegisterFragment: Fragment() {
+class CommunityPostRegisterFragment : Fragment() {
+
+    private val viewModel: CommunityPostRegisterViewModel by viewModels { ViewModelFactory() }
     private lateinit var binding: FragmentCommunityPostRegisterBinding
 
     override fun onCreateView(
@@ -22,5 +26,23 @@ class CommunityPostRegisterFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        setObserve()
+        setChallenge()
+    }
+
+    private fun setObserve() {
+        viewModel.isChallenge.observe(
+            viewLifecycleOwner
+        ) {
+            binding.isChallenge = it
+        }
+    }
+
+    private fun setChallenge() {
+        binding.communityPostRegisterChallengeTv.setOnClickListener {
+            viewModel.setIsChallenge()
+        }
     }
 }
