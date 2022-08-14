c067e1bf1ea4f2c0c02ac07fd0f9a187927a8fb3
SunWoong <sunwoong1122@gmail.com>
2022-08-14 17:55:06 +0900

    feat: HttpLoggingInterceptor 요청/응답 log 출력 기능 구현

--- app/src/main/java/com/greenfriends/zeroway/network/RetrofitClient.kt
+++ app/src/main/java/com/greenfriends/zeroway/network/RetrofitClient.kt
@@ -1,5 +1,7 @@
 package com.greenfriends.zeroway.network
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -9,8 +11,16 @@ object RetrofitClient {
 
     fun getRetrofit(): Retrofit? {
         if (retrofit == null) {
+            val logger = HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BASIC
+            }
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logger)
+                .build()
+
             retrofit = Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .client(client)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
         }
