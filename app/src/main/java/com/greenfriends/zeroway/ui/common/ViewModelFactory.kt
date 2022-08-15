6e4f90cb5d40972e3ee2401fce9363cd9a56cc10
SunWoong <sunwoong1122@gmail.com>
2022-08-15 17:10:23 +0900

    feat: community view model 추가

--- app/src/main/java/com/greenfriends/zeroway/ui/common/ViewModelFactory.kt
+++ app/src/main/java/com/greenfriends/zeroway/ui/common/ViewModelFactory.kt
@@ -2,21 +2,30 @@ package com.greenfriends.zeroway.ui.common
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.greenfriends.zeroway.repository.community.CommunityDataSourceImpl
 import com.greenfriends.zeroway.repository.community.CommunityPostRegisterDataSourceImpl
 import com.greenfriends.zeroway.repository.community.CommunityPostRegisterRepository
+import com.greenfriends.zeroway.repository.community.CommunityRepository
 import com.greenfriends.zeroway.ui.community.CommunityPostRegisterViewModel
+import com.greenfriends.zeroway.ui.community.CommunityViewModel
 
 class ViewModelFactory : ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(CommunityPostRegisterViewModel::class.java)) {
-            return CommunityPostRegisterViewModel(
-                CommunityPostRegisterRepository(
-                    CommunityPostRegisterDataSourceImpl()
-                )
-            ) as T
-        } else {
-            throw java.lang.IllegalArgumentException("Failed to create ViewModel: ${modelClass.name}")
+        when {
+            modelClass.isAssignableFrom(CommunityPostRegisterViewModel::class.java) -> {
+                return CommunityPostRegisterViewModel(
+                    CommunityPostRegisterRepository(
+                        CommunityPostRegisterDataSourceImpl()
+                    )
+                ) as T
+            }
+            modelClass.isAssignableFrom(CommunityViewModel::class.java) -> {
+                return CommunityViewModel(CommunityRepository(CommunityDataSourceImpl())) as T
+            }
+            else -> {
+                throw java.lang.IllegalArgumentException("Failed to create ViewModel: ${modelClass.name}")
+            }
         }
     }
 }
