90174967a41ac184f33a45739f784c6e308bcb92
cyjadela <adelacho@naver.com>
2022-08-18 18:58:55 +0900

    feat: 챌린지 list API 연동

--- app/src/main/java/com/greenfriends/zeroway/network/ChallengeService.kt
+++ app/src/main/java/com/greenfriends/zeroway/network/ChallengeService.kt
@@ -5,10 +5,7 @@ import com.greenfriends.zeroway.api.StoreListView
 import com.greenfriends.zeroway.api.TermSearchView
 import com.greenfriends.zeroway.api.TermView
 import com.greenfriends.zeroway.api.TipView
-import com.greenfriends.zeroway.model.ChallengeResponse
-import com.greenfriends.zeroway.model.StoreResponse
-import com.greenfriends.zeroway.model.TermResponse
-import com.greenfriends.zeroway.model.TipResponse
+import com.greenfriends.zeroway.model.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -16,9 +13,14 @@ import retrofit2.Response
 class ChallengeService {
     private var retrofit = RetrofitClient.getRetrofit()
     private lateinit var challengeView: ChallengeView
+    private lateinit var challengeListView: ChallengeListView
 
     fun setChallengeView(challengeView: ChallengeView) {
         this.challengeView = challengeView
+    }
+
+    fun setChallengeListView(challengeListView: ChallengeListView) {
+        this.challengeListView = challengeListView
     }
 
     fun getChallenge(token: String) {
@@ -44,4 +46,26 @@ class ChallengeService {
         })
     }
 
+    fun getChallengeList(token: String) {
+        val challengeService = retrofit?.create(ChallengeRetrofitInterface::class.java)
+        challengeService!!.getChallengeList(token).enqueue(object : Callback<List<ChallengeListResponse>> {
+
+            override fun onResponse(
+                call: Call<List<ChallengeListResponse>>,
+                response: Response<List<ChallengeListResponse>>
+            ) {
+                Log.d("CHALLENGE LIST/SUCCESS", response.toString())
+                if (response.isSuccessful) {
+                    challengeListView.onChallengeListSuccess(response.body()!!)
+                } else {
+                    challengeListView.onChallengeListFailure()
+                }
+            }
+
+            override fun onFailure(call: Call<List<ChallengeListResponse>>, t: Throwable) {
+                Log.d("CHALLENGE LIST/FAILURE", t.message.toString())
+            }
+
+        })
+    }
 }
--- app/src/main/java/com/greenfriends/zeroway/ui/challenge/ChallengeListFragment.kt
+++ app/src/main/java/com/greenfriends/zeroway/ui/challenge/ChallengeListFragment.kt
@@ -4,15 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.greenfriends.zeroway.R
 import com.greenfriends.zeroway.databinding.FragmentChallengeListBinding
 import com.greenfriends.zeroway.model.ChallengeListResponse
 import com.greenfriends.zeroway.model.ShopList
+import com.greenfriends.zeroway.model.UseList
+import com.greenfriends.zeroway.network.ChallengeListView
+import com.greenfriends.zeroway.network.ChallengeService
 import com.greenfriends.zeroway.ui.ShopAdapter
 
-class ChallengeListFragment : Fragment() {
+class ChallengeListFragment : Fragment(),ChallengeListView {
     private lateinit var binding: FragmentChallengeListBinding
 
     private var challengeList = ArrayList<ChallengeListResponse>()
@@ -24,15 +28,37 @@ class ChallengeListFragment : Fragment() {
     ): View? {
         binding = FragmentChallengeListBinding.inflate(inflater, container, false)
 
-        challengeList.apply {
-            add(ChallengeListResponse(1,"cici",false))
-            add(ChallengeListResponse(2,"icic",true))
+        getChallengeList(getJwt()!!)
+
+        return binding.root
+    }
+
+    private fun getChallengeList(token: String) {
+        val challengeListService = ChallengeService()
+        challengeListService.setChallengeListView(this)
+        challengeListService.getChallengeList(token)
+    }
+
+    override fun onChallengeListSuccess(result: List<ChallengeListResponse>) {
+        for (i in result){
+            challengeList.add(i)
         }
+
         val challengeListAdapter = ChallengeListAdapter(challengeList)
         binding.challengeListRv.adapter = challengeListAdapter
         binding.challengeListRv.layoutManager =
             LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
 
-        return binding.root
     }
+
+    override fun onChallengeListFailure() {
+        TODO("Not yet implemented")
+    }
+
+    private fun getJwt(): String? {
+        val sharedPreferences =
+            activity?.getSharedPreferences("auth", AppCompatActivity.MODE_PRIVATE)
+        return sharedPreferences!!.getString("jwt", null)
+    }
+
 }
