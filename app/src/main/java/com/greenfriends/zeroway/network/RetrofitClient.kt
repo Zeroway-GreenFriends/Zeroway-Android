adbcfaf43f44ebf9d9ed1209f7aec4a6493e581b
cyjadela <adelacho@naver.com>
2022-08-20 19:12:41 +0900

    feat: 레벨업 화면 기능 구현

--- app/src/main/java/com/greenfriends/zeroway/network/RetrofitClient.kt
+++ app/src/main/java/com/greenfriends/zeroway/network/RetrofitClient.kt
@@ -4,6 +4,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
     private const val BASE_URL = "https://www.zeroway.site/api/"
@@ -16,6 +17,9 @@ object RetrofitClient {
             }
             val client = OkHttpClient.Builder()
                 .addInterceptor(logger)
+//                .connectTimeout(300, TimeUnit.SECONDS)
+//                .readTimeout(300,TimeUnit.SECONDS)
+//                .writeTimeout(300,TimeUnit.SECONDS)
                 .build()
 
             retrofit = Retrofit.Builder()
--- app/src/main/java/com/greenfriends/zeroway/ui/challenge/LevelUpActivity.kt
+++ app/src/main/java/com/greenfriends/zeroway/ui/challenge/LevelUpActivity.kt
@@ -2,6 +2,7 @@ package com.greenfriends.zeroway.ui.challenge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.greenfriends.zeroway.R
 import com.greenfriends.zeroway.databinding.ActivityLevelupBinding
 import com.greenfriends.zeroway.databinding.ActivityMainBinding
 import com.greenfriends.zeroway.ui.challenge.ChallengeCharacterFragment
@@ -17,8 +18,48 @@ class LevelUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLevelupBinding.inflate(layoutInflater)
 
+        binding.levelupLevelTv.text = "Lv."+getLevel().toString()
+
+        when(getLevel()){
+            "2" ->{
+                binding.levelupCharacterIv.setImageResource(R.drawable.ic_level_up_2)
+            }
+            "3" ->{
+                binding.levelupCharacterIv.setImageResource(R.drawable.ic_level_up_3)
+            }
+            "4" ->{
+                binding.levelupCharacterIv.setImageResource(R.drawable.ic_level_up_4)
+            }
+            "5" ->{
+                binding.levelupCharacterIv.setImageResource(R.drawable.ic_level_up_5)
+            }
+            "6" ->{
+                binding.levelupCharacterIv.setImageResource(R.drawable.ic_level_up_6)
+            }
+            "7" ->{
+                binding.levelupCharacterIv.setImageResource(R.drawable.ic_level_up_7)
+            }
+            "8" ->{
+                binding.levelupCharacterIv.setImageResource(R.drawable.ic_level_up_8)
+            }
+            "9" ->{
+                binding.levelupCharacterIv.setImageResource(R.drawable.ic_level_up_9)
+            }
+            "10" ->{
+                binding.levelupCharacterIv.setImageResource(R.drawable.ic_level_up_10)
+            }
+        }
+
+        binding.levelupOkBtn.setOnClickListener {
+            finish()
+        }
 
         setContentView(binding.root)
+    }
+
+    private fun getLevel(): String? {
+        val sharedPreferences = getSharedPreferences("level", MODE_PRIVATE)
+        return sharedPreferences!!.getString("level", null)
     }
 
 }
