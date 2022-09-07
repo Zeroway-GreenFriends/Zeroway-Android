a8a3754376276f2c2da4bc99cfbfe961d3e12448
SunWoong <sunwoong1122@gmail.com>
2022-09-08 02:43:42 +0900

    feat: 회원가입 API 수정 & 닉네임 중복 확인 API 구현

--- app/src/main/java/com/greenfriends/zeroway/data/api/AuthRetrofitInterface.kt
+++ app/src/main/java/com/greenfriends/zeroway/data/api/AuthRetrofitInterface.kt
@@ -1,14 +1,13 @@
 package com.greenfriends.zeroway.data.api
 
 import com.greenfriends.zeroway.data.model.AuthResponse
+import com.greenfriends.zeroway.data.model.IdCheckResponse
 import com.greenfriends.zeroway.data.model.LoginRequest
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.Multipart
-import retrofit2.http.POST
-import retrofit2.http.Part
+import retrofit2.Response
+import retrofit2.http.*
 
 interface AuthRetrofitInterface {
     @POST("user/auth/login")
@@ -16,10 +15,21 @@ interface AuthRetrofitInterface {
         @Body loginRequest: LoginRequest
     ): Call<AuthResponse>
 
+    /**
+     * 회원가입 API
+     */
     @Multipart
     @POST("user")
-    fun signUp(
+    suspend fun signUp(
         @Part profileImg: MultipartBody.Part,
         @Part("signInReq") user: RequestBody
-    ): Call<AuthResponse>
+    ): Response<AuthResponse>
+
+    /**
+     * 닉네임 중복 확인 API
+     */
+    @GET("user/{nickname}")
+    suspend fun idCheck(
+        @Path("nickname") nickname: String
+    ): Response<IdCheckResponse>
 }
