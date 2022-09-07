2351e0a40f6e0fb9aa698f59281339bb6bad81cb
SunWoong <sunwoong1122@gmail.com>
2022-09-07 14:44:37 +0900

    feat: 제로 웨이스트 샵 조회 API 구현

--- app/src/main/java/com/greenfriends/zeroway/data/api/StoreRetrofitInterface.kt
+++ app/src/main/java/com/greenfriends/zeroway/data/api/StoreRetrofitInterface.kt
@@ -2,7 +2,9 @@ package com.greenfriends.zeroway.data.api
 
 import com.greenfriends.zeroway.data.model.StoreResponse
 import retrofit2.Call
-import retrofit2.http.*
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface StoreRetrofitInterface {
 
@@ -12,4 +14,14 @@ interface StoreRetrofitInterface {
         @Query("page") page: Int? = null,
         @Query("size") size: Int? = null,
     ): Call<List<StoreResponse>>
+
+    /**
+     * 제로 웨이스트 샵 조회 API
+     */
+    @GET("shop/list")
+    suspend fun getStores(
+        @Query("keyword") keyword: String?,
+        @Query("page") page: Int,
+        @Query("size") size: Int
+    ): Response<List<StoreResponse>>
 }
