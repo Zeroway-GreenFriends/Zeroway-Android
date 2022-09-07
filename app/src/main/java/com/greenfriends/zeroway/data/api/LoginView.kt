0eeae16bdc8902753fe6d435031eb2a5168998ef
SunWoong <sunwoong1122@gmail.com>
2022-09-08 02:42:25 +0900

    feat: result data class 수정 & id check data class 추가

--- app/src/main/java/com/greenfriends/zeroway/data/api/LoginView.kt
+++ app/src/main/java/com/greenfriends/zeroway/data/api/LoginView.kt
@@ -1,8 +1,8 @@
 package com.greenfriends.zeroway.data.api
 
-import com.greenfriends.zeroway.data.model.Result
+import com.greenfriends.zeroway.data.model.UserIdentification
 
 interface LoginView {
-    fun onLoginSuccess(result: Result)
+    fun onLoginSuccess(result: UserIdentification)
     fun onLoginFailure()
 }
