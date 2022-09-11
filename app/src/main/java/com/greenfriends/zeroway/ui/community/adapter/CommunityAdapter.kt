4ddd4a2fba61ad6b899cbb92c4d09938fa989564
SunWoong <sunwoong1122@gmail.com>
2022-09-11 20:16:30 +0900

    refactor: list adapter -> recycler view adapter 변경 & loading 기능 구현

--- app/src/main/java/com/greenfriends/zeroway/ui/community/adapter/CommunityAdapter.kt
+++ app/src/main/java/com/greenfriends/zeroway/ui/community/adapter/CommunityAdapter.kt
@@ -1,28 +1,35 @@
 package com.greenfriends.zeroway.ui.community.adapter
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Build
+import android.util.Log
 import android.view.ContextThemeWrapper
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.PopupMenu
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.greenfriends.zeroway.R
 import com.greenfriends.zeroway.data.model.CommunityPost
 import com.greenfriends.zeroway.databinding.ItemCommunityExcludeImagePostBinding
 import com.greenfriends.zeroway.databinding.ItemCommunityIncludeImagePostBinding
+import com.greenfriends.zeroway.databinding.ItemLoadingBinding
+import com.greenfriends.zeroway.ui.common.LoadingViewHolder
 import com.greenfriends.zeroway.ui.community.OnCommunityItemClickListener
 import com.greenfriends.zeroway.ui.community.viewmodel.CommunityViewModel
+
+private const val VIEW_TYPE_LOADING = 0
+private const val VIEW_TYPE_IMAGE_POST = 1
+private const val VIEW_TYPE_POST = 2
 
 class CommunityAdapter(
     private val viewModel: CommunityViewModel,
     private val context: Context
 ) :
-    ListAdapter<CommunityPost, RecyclerView.ViewHolder>(CommunityDiffCallback()) {
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
+    private val communityPosts = mutableListOf<CommunityPost?>()
     private lateinit var onCommunityItemClickListener: OnCommunityItemClickListener
 
     fun setOnCommunityItemClickListener(onCommunityItemClickListener: OnCommunityItemClickListener) {
@@ -30,33 +37,69 @@ class CommunityAdapter(
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return if (viewType == 1) {
-            val binding = ItemCommunityIncludeImagePostBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent,
-                false
-            )
-            CommunityIncludeImageViewHolder(binding)
-        } else {
-            val binding = ItemCommunityExcludeImagePostBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent,
-                false
-            )
-            CommunityExcludeImageViewHolder(binding)
+        val inflater = LayoutInflater.from(parent.context)
+        return when (viewType) {
+            VIEW_TYPE_LOADING -> {
+                LoadingViewHolder(
+                    ItemLoadingBinding.inflate(inflater, parent, false)
+                )
+            }
+            VIEW_TYPE_IMAGE_POST -> {
+                CommunityIncludeImageViewHolder(
+                    ItemCommunityIncludeImagePostBinding.inflate(inflater, parent, false)
+                )
+            }
+            else -> {
+                CommunityExcludeImageViewHolder(
+                    ItemCommunityExcludeImagePostBinding.inflate(inflater, parent, false)
+                )
+            }
         }
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        if (getItemViewType(position) == 1) {
-            (holder as CommunityIncludeImageViewHolder).bind(getItem(position))
-        } else {
-            (holder as CommunityExcludeImageViewHolder).bind(getItem(position))
+        when (holder) {
+            is CommunityIncludeImageViewHolder -> holder.bind(communityPosts[position]!!)
+            is CommunityExcludeImageViewHolder -> holder.bind(communityPosts[position]!!)
         }
     }
 
     override fun getItemViewType(position: Int): Int {
-        return if (getItem(position).imageList.isNotEmpty()) 1 else 0
+        return when (communityPosts[position]) {
+            null -> VIEW_TYPE_LOADING
+            else -> {
+                if (communityPosts[position]!!.imageList.isNotEmpty()) VIEW_TYPE_IMAGE_POST else VIEW_TYPE_POST
+            }
+        }
+    }
+
+    override fun getItemCount(): Int = communityPosts.size
+
+    fun submitCommunityPosts(communityPosts: List<CommunityPost>) {
+        this.communityPosts.addAll(communityPosts)
+        notifyItemRangeInserted(itemCount, communityPosts.size)
+    }
+
+    fun setLoading(isLoading: Boolean): Boolean {
+        return when (isLoading) {
+            true -> {
+                communityPosts.add(null)
+                notifyItemInserted(itemCount - 1)
+                true
+            }
+            else -> {
+                communityPosts.removeAt(itemCount - 1)
+                notifyItemRemoved(itemCount)
+                Log.d("SSS", communityPosts.toString())
+                true
+            }
+        }
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun clear() {
+        communityPosts.clear()
+        notifyDataSetChanged()
     }
 
     inner class CommunityIncludeImageViewHolder(val binding: ItemCommunityIncludeImagePostBinding) :
@@ -188,16 +231,5 @@ class CommunityAdapter(
                 }
             }
         }
-    }
-}
-
-class CommunityDiffCallback : DiffUtil.ItemCallback<CommunityPost>() {
-
-    override fun areItemsTheSame(oldItem: CommunityPost, newItem: CommunityPost): Boolean {
-        return oldItem.postId == newItem.postId
-    }
-
-    override fun areContentsTheSame(oldItem: CommunityPost, newItem: CommunityPost): Boolean {
-        return oldItem == newItem
     }
 }
