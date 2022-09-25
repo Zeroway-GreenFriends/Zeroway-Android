778f705bed20ed41a9a8221a05eb551a8189a352
SunWoong <sunwoong1122@gmail.com>
2022-09-25 17:24:09 +0900

    fix: 제로 웨이스트 샵 data class 추가 & 수정

--- app/src/main/java/com/greenfriends/zeroway/data/model/Store.kt
+++ app/src/main/java/com/greenfriends/zeroway/data/model/Store.kt
@@ -3,13 +3,25 @@ package com.greenfriends.zeroway.data.model
 import com.google.gson.annotations.SerializedName
 
 data class StoreResponse(
-    @SerializedName("imageUrl") val imageUrl: String,
+    @SerializedName("storeId") val storeId: Long,
+    @SerializedName("imageUrl") val imageUrl: String?,
     @SerializedName("name") val name: String,
-    @SerializedName("item") val item: String,
-    @SerializedName("scoreAvg") val scoreAvg: Double,
+    @SerializedName("item") val item: String?,
     @SerializedName("addressNew") val addressNew: String,
-    @SerializedName("operatingTime") val operatingTime: String,
-    @SerializedName("contact") val contact: String,
-    @SerializedName("siteUrl") val siteUrl: String,
-    @SerializedName("instagram") val instagram: String
+    @SerializedName("operatingTime") val operatingTime: String?,
+    @SerializedName("contact") val contact: String?,
+    @SerializedName("siteUrl") val siteUrl: String?,
+    @SerializedName("instagram") val instagram: String?
+)
+
+data class StorePostDetailResponse(
+    @SerializedName("imageUrl") val imageUrl: String?,
+    @SerializedName("name") val name: String,
+    @SerializedName("item") val item: String?,
+    @SerializedName("addressNew") val addressNew: String,
+    @SerializedName("operatingTime") val operatingTime: String?,
+    @SerializedName("contact") val contact: String?,
+    @SerializedName("siteUrl") val siteUrl: String?,
+    @SerializedName("instagram") val instagram: String?,
+    @SerializedName("description") val description: String?
 )
